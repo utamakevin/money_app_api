0e6a67e145a5a83eef27369c4a97f0f87928a3c7
utamakevin <utama.kevin@yahoo.com>
2023-05-02 10:37:39 +0700

    update connection string

--- config/index.js
+++ config/index.js
@@ -9,10 +9,10 @@ if (missing.length > 0) {
 }
 
 const deployment = {
-  // prod: {
-  //   connectionString:
-  //     "postgres://pokequiz_user:hq0k9ErHGq2gL1fbbRi1sWBiWQTSwbmX@dpg-ch2uevt269v61ffhh5dg-a/pokequiz",
-  // },
+  prod: {
+    connectionString:
+      "postgres://money_app_user:zqLqHMUWfmOXKAL6ioqLyKOh5zmIx7WM@dpg-ch7s455gk4q7lmu02u2g-a/money_app",
+  },
   dev: {
     connectionString: process.env.DATABASE_URL,
   },
